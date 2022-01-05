ae241507ae71aafa1c7083024925ade4131a14d6
Nguyễn Ngọc Huy <92006039+Ngochuy2137@users.noreply.github.com>
2022-01-05 18:03:31 +0700

    From VT

--- src/safety_checker/src/safety_checker.cpp
+++ src/safety_checker/src/safety_checker.cpp
@@ -1,81 +1,106 @@
 #include <nav_msgs/Path.h>
-#include<geometry_msgs/PoseStamped.h>
-#include "ros/ros.h"
+#include <nav_msgs/OccupancyGrid.h>
+// #include<geometry_msgs/PoseStamped.h>
+// #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <iostream>
+// #include <iostream>
 #include <typeinfo>
+// #include <costmap_2d/costmap_2d.h>
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <tf2_ros/transform_listener.h>
+// #include <tf2_ros/buffer.h>
+// #include <tf2_ros/buffer_client.h>
+// #include <tf2_ros/buffer_interface.h>
+// #include <tf2_ros/buffer_server.h>
+#include<thread>
+
+// undefined reference to `vtable for tf2_ros::Buffer'
+#include <ros/duration.h>
+
 
 using namespace std;
+struct Point
+{
+  float x;
+  float y;
+};
+struct localPoint
+{
+  unsigned int x;
+  unsigned int y;
+  unsigned char cost;
+};
 
-// class path_poses
-// {
-// public:
-//   int num_poses = 3000;
-//   // geometry_msgs::PoseStamped poses[num_poses];
-//   // bool check_obstacles(geometry_msgs::PoseStamped checked_poses);
-//   // path_poses();
-//   // ~path_poses();
-// };
-int num_poses = 3000;
-// geometry_msgs::PoseStamped path_pose[num_poses];
+class costPose
+{
+public:
+  Point point;
+  localPoint local_point;
+};
 
-// void path_poses::check_obstacles(geometry_msgs::PoseStamped checked_pose)
-// {
-  
-// }
+const int num_poses = 100;
+
 nav_msgs::Path checked_path;
-geometry_msgs::PoseStamped checked_pose;
+// geometry_msgs::PoseStamped checked_pose;
+costPose cost_pose[num_poses];
 
-
+float CHECK = 10;
 void get_path_cb(const nav_msgs::Path::ConstPtr& msg)
 {
   // ROS_INFO("Checking path=================================");
 
-  //  ĐẾM SỐ CELLS TRONG PATH
+  //  TO-DO: 
+    // ĐẾM SỐ CELLS TRONG PATH
     // cout << "size *msg:" << sizeof(*msg) << endl;
     // cout << "size msg->poses[0]:" << sizeof(msg->poses[0]) << endl;
-
     // cout << msg->poses[0].pose << endl;
+
   checked_path = *msg;
   
-  cout << checked_path << endl;
-  for(int i = 0; i<100; i++)
+  for(int i = 0; i<num_poses; i++)
   {
-    cout << "=========" << i << "=========" << endl;
-    cout << checked_path.poses[i] << endl;
-    // checked_pose
+    // cout << "=========" << i << "=========" << endl;
+    // cout << checked_path.poses[i].pose.position.x << endl;
+    cost_pose[i].point.x = checked_path.poses[i].pose.position.x;
+    cost_pose[i].point.y = checked_path.poses[i].pose.position.y;
   }
-
-
-  // nav_msgs::Path msg2;
-
-    // ROS_INFO("I heard: [%s]", msg->header);
-
-    // SAMPLE
-    // cout << typeid(msg->data).name() << endl;
+  cout << " pose" << cost_pose[1].point.x << " " << cost_pose[1].point.y << endl;
   
-    // cout << typeid(msg->data).name() << endl;
-    
-    // ROS_INFO("I heard: [%s]", msg->data.c_str());
-
-    // cout << "size *msg:" << sizeof(*msg) << endl;
 }
-
-// void safety_checker_cb()
-// {
-
-// }
-
-// bool check obs_on_path();
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "safety_checker");
   ros::NodeHandle n;
   ros::Subscriber sub_path = n.subscribe("/move_base/NavfnROS/plan", 1, get_path_cb);
-
   // ros::Subscriber sub_costmap = n.subscribe("/move_base/local_costmap/costmap", 1, safety_checker_cb);
-  ros::spin();
+  tf2_ros::Buffer buffer(ros::Duration(10));
+  tf2_ros::TransformListener tf(buffer);
+
+  costmap_2d::Costmap2DROS safety_costmap("safety_costmap", buffer);
+  costmap_2d::Costmap2D* costmap = safety_costmap.getCostmap();
+  int count = 0;
+  while(ros::ok())
+  {
+    
+    //get a copy of the costmap contained by our ros wrapper
+    for(int i = 0; i<num_poses; i++)
+    {
+      costmap->worldToMap(cost_pose[i].point.x, cost_pose[i].point.y, cost_pose[i].local_point.x,cost_pose[i].local_point.y);
+      cost_pose[i].local_point.cost = costmap->getCost(cost_pose[i].local_point.x, cost_pose[i].local_point.y);
+    }
+    cout << "Pose " << cost_pose[10].point.x << " " << cost_pose[10].point.y << endl;
+    cout << "local Pose " << cost_pose[10].local_point.x << " " << cost_pose[10].local_point.y << endl;
+    // cout << "COST " << cost_pose[10].local_point.cost << endl;
+    // for(int i=0; i< num_poses; i++)
+    // {
+    //   cout << "OUT " << cost_pose[1].point.x << " " << cost_pose[1].point.y << endl;
+    // }
+    count ++;
+    ros::spinOnce();
+    ros::Duration(1.0).sleep();
+  }
 
   return 0;
 }
